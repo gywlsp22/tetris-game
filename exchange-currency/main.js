0244063518e99adb1399eac1201099858101e673
hyojin <dlsvl98@daum.net>
2022-05-14 22:27:09 +0900

    Add exchange-currency file

---
+++ exchange-currency/main.js
@@ -0,0 +1,18 @@
+let currencyRatio = {
+  USD: {
+    KRW: 1278.56,
+    USD: 1,
+    VND: 23087.50
+  },
+  KRW: {
+    KRW: 1,
+    USD: 0.00078,
+    VND: 18.06
+  },
+  VND: {
+    USD: 0.000043,
+    VND: 1,
+    KRW: 0.055
+  }
+
+}
